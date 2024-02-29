ed8b85f9f113ff496b7f5acc74553546fde30c08
Albert Kondakov <nev3n4ik.yt@gmail.com>
2024-02-29 20:04:11 +0700

    Added all files

---
+++ app.js
@@ -0,0 +1,72 @@
+// let num = 42
+// let firstName = 'Albert'
+// const isProgrammer = true
+
+// Can do
+// let $ = 'test'
+// let $num = 42
+// let num$ = 42
+// let _ = 49
+// let _num = 12
+// let num_ = 12
+// let first_name = 'Elena' // bad
+// let myNum = 34 // good
+// let num42 = 10
+
+// Restricted
+// let 42num = '11'
+// let my-num = 1
+// let let = 'let'
+// let true = true
+
+// alert(firstName)
+// console.log('Test:', isProgrammer, num);
+
+// const fullName = firstName + ' Kondakov'
+// console.log(fullName);
+
+const resultElement = document.getElementById('result')
+const input1 = document.getElementById('input1')
+const input2 = document.getElementById('input2')
+const submitBtn = document.getElementById('submit')
+const plusBtn = document.getElementById('plus')
+const minusBtn = document.getElementById('minus')
+const multuplyBtn = document.getElementById('multiply')
+const divisionBtn = document.getElementById('division')
+let mathAct
+
+plusBtn.onclick = () => {
+    mathAct = '+'
+}
+minusBtn.onclick = () => {
+    mathAct = '-'
+}
+multuplyBtn.onclick = () => {
+    mathAct = '*'
+}
+divisionBtn.onclick = () => {
+    mathAct = ':'
+}
+
+function computeNumbersWithAction(inp1, inp2, action) {
+    const val1 = Number(inp1.value)
+    const val2 = Number(inp2.value)
+    if (action == '+') {
+        return val1 + val2
+    } else if (action == '+') {
+        return val1 - val2
+    }
+    // return action == '+' ? val1 + val2 : val1 - val2
+}
+function printResult(res) {
+    if (res < 0) {
+        resultElement.style.color = 'red'
+    } else {
+        resultElement.style.color = 'green'
+    }
+    resultElement.textContent = res
+}
+submit.onclick = function () {
+    const result = computeNumbersWithAction(input1, input2, mathAct)
+    printResult(result)
+}
